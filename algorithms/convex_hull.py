47c9fdbc5137c648bddac8d6a98497cf09111c31
Illarion Khlestov <khlyestovillarion@gmail.com>
2020-12-14 15:19:04 +0200

    Implemented naive convex hull algorithm

--- algorithms/convex_hull.py
+++ algorithms/convex_hull.py
@@ -1,8 +1,23 @@
+from .predicates import points_turn_value
+
 def naive(points):
-    result = []
-    for idx in range(len(points)):
-        result.append((points[idx], points[idx % len(points)]))
-    return result
+    edges = []
+    for p1 in points:
+        for p2 in points:
+            points_are_edge = True
+            if p1 == p2:
+                continue
+            for p3 in points:
+                if p3 == p1 or p3 == p2:
+                    continue
+                turn = points_turn_value(p1, p2, p3)
+                if turn >= 0:
+                    points_are_edge = False
+                    break
+            if points_are_edge:
+                edges.append([p1, p2])
+    return edges
+
 
 def graham(points):
     result = []
